e5a529d3156ebf1e6b362f316785422c75536ec4
devnewton <devnewton@bci.im>
2017-05-05 12:05:31 +0200

    utf8 slip (to be tested)

--- post.php
+++ post.php
@@ -3,10 +3,19 @@
 include_once 'common.php';
 
 $lastId = filter_input(INPUT_POST, 'lastId', FILTER_VALIDATE_INT, array('options' => array('default' => 0)));
-$message = substr(filter_input(INPUT_POST, 'message', FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW), 0, TAAB_MAX_POST_LENGTH);
-$login = trim(substr(filter_input(INPUT_POST, 'login', FILTER_SANITIZE_EMAIL), 0, TAAB_MAX_LOGIN_LENGTH));
-$info = trim(substr(filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_SANITIZE_EMAIL), 0, TAAB_MAX_INFO_LENGTH));
-if (strlen($login) === 0 && strlen($info) === 0) {
+$message = mb_substr(filter_input(INPUT_POST, 'message', FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW), 0, TAAB_MAX_POST_LENGTH);
+$login = trim(mb_substr(filter_input(INPUT_POST, 'login', FILTER_SANITIZE_EMAIL), 0, TAAB_MAX_LOGIN_LENGTH));
+$info = trim(mb_substr(filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_SANITIZE_EMAIL), 0, TAAB_MAX_INFO_LENGTH));
+
+if(!mb_detect_encoding($login, 'UTF-8', true)) {
+    $login = "relou";
+}
+
+if(!mb_detect_encoding($info, 'UTF-8', true)) {
+    $info = "relou";
+}
+
+if (mb_strlen($login) === 0 && mb_strlen($info) === 0) {
     $info = "coward";
 }
 
@@ -18,7 +27,7 @@ while (($post = fgetcsv($file, TAAB_MAX_BACKEND_LINE_LENGTH, "\t")) !== FALSE) {
     $newPosts[] = $post;
     $newPostId = max($newPostId, $post[0]);
 }
-if (strlen(trim($message)) > 0) {
+if (mb_strlen(trim($message)) > 0 && mb_detect_encoding($message, 'UTF-8', true)) {
     ++$newPostId;
     $dateTime = date_create("now", timezone_open("Europe/Paris"));
     $time = date_format($dateTime, 'YmdHis');
