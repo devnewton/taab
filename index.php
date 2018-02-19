3dc24a3f1e51622350511aa81f28cce7a1d76831
devnewton <devnewton@bci.im>
2018-02-19 12:52:26 +0100

    add meta trucs

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="noindex,nofollow">
         <title>taab</title>
         <link rel="stylesheet" href="css/blaze.css">
         <link rel="stylesheet" href="css/taab.css">
